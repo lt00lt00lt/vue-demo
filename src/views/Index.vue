875c8cc5a48fd5509e3d1aa0e6ae75ddf3a67914
lt00lt00lt <494858549@qq.com>
2019-12-02 13:42:03 +0800

    '创建新组建'

---
+++ src/views/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Vue" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld";
+
+export default {
+  name: "index",
+  components: {
+    HelloWorld
+  }
+};
+</script>
